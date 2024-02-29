5cab5648c6a27c1eba9d03cff99bc07f7af42042
Jules <thomas.desessarts-jules@hotmail.com>
2024-02-29 17:46:18 +0100

    WIP sidebar data style

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -2,11 +2,16 @@
     <div>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Recent searches</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
-                ...
+                <div class="searched">
+                    <div>Houilles - 78</div>
+                    <div>
+                        <font-awesome-icon :icon="['fas', 'trash-can']" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -30,5 +35,20 @@ export default {
 }
 </script>
   
-<style scoped></style>
+<style scoped>
+.searched {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    border: 1px solid #20c997;
+    border-radius: 0.5rem;
+}
+
+
+.searched:hover {
+    background-color: #17a2b8;
+}
+</style>
   
