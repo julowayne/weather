9b6a880a7ffc01ea72543f375e3aa00271f98748
Jules <thomas.desessarts-jules@hotmail.com>
2024-06-03 15:11:50 +0200

    WIP forecast

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   "rules": {
     "vue/multi-word-component-names": ["error", {
-      "ignores": ["Search", "Home", "Forecast", "Toasters", "Today", "Toaster"] 
+      "ignores": ["Search", "Home", "Forecast", "Toasters", "Today", "Toaster", "Chart"] 
     }],
   }
 }
---
+++ src/components/chart.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <canvas id="forecastTemperatures"></canvas>
+    </div>
+</template>
+
+<script lang="ts">
+import Chart from 'chart.js';
+import { mapStores } from 'pinia'
+import { useSearchStore } from '@/stores/search';
+
+
+export default {
+    name: 'Chart',
+    computed: {
+        ...mapStores(useSearchStore),
+    },
+
+    methods: {
+        graphic() {
+
+        }
+    },
+
+    mounted() {
+        this.graphic()
+    },
+}
+</script>
+
+<style scoped></style>
